6c5341d4c1f7db5b935af5af1cc95f870e120f74
Alicja Jagielska <alicja_dubai@wp.pl>
2021-11-18 18:19:12 +0100

    PlayerMove

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+//losowanie ruchu komputera
+
 var computerMove, PlayerMove;
 PlayerMove = 0;
 PlayerMove = (typeof PlayerMove == 'number' ? PlayerMove : 0) + 1;
@@ -5,3 +7,25 @@ computerMove = 'kamień';
 printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
 PlayerMove = 'Papier';
 printMessage('Zagralem' + PlayerMove + '!Jesli Twoj ruch to kamien, to wygrywasz!');
+
+//ustalenie ruchu gracza
+var playerInput, playerMove;
+
+
+1 + 1;
+
+1 + 1;
+
+playerMove;
+playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+console.log('Wpisana odpowiedź to: ' + playerInput);
+if (playerInput == '1') {
+  playerMove = 'kamień';
+} else if (playerInput == '2') {
+  playerMove = 'papier';
+} else if (playerInput == '3') {
+  playerMove = 'nozyce';
+} else {
+  playerMove = 'nieznany ruch';
+}
+printMessage('Twój ruch: ' + playerMove);
